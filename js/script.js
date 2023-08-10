2ace15f3b37d94e59f579bae0734a82b6e54b783
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2023-08-10 12:47:38 -0500

    fix js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-// Hide Nav on Scroll
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-// var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-100px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+$(function(){  // $(document).ready shorthand
+    $('#home').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.my-fade').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
